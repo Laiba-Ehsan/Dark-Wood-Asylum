1d8048bbbf8fa903191096d03a031ca63b33b0a7
elli560 <waaniawaseem777@gmail.com>
2024-07-30 14:33:26 +0500

    Patrol state plus arrow shooting
    erika is done plus dragon patrol state

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,8 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool isAiming;
+		public bool isShooting;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -21,6 +23,7 @@ namespace StarterAssets
 		public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
+
 		public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
@@ -43,6 +46,17 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+		public void OnAiming(InputValue value)
+		{
+            isAiming = value.isPressed;
+        }
+
+		public void OnShoot(InputValue value)
+		{
+			isShooting = value.isPressed;
+		}
+
+			
 #endif
 
 
