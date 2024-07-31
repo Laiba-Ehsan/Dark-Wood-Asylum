b5b8db7e743052e77da562f9bb2bf8b167ff8b6b
Laiba-Ehsan <laibaehsan987@gmail.com>
2024-07-31 23:21:22 +0500

    Health Bar

--- Assets/Dragon.cs
+++ Assets/Dragon.cs
@@ -1,22 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dragon : MonoBehaviour
 {
-    public int HP = 100;
+    private int HP = 100;
+    public Slider healthBar;
+
     public Animator animator;
+
+    void Update()
+    {
+        healthBar.value = HP;
+    }
+
+
+  
+
     public void TakeDamage(int damageAmount)
     {
         HP -= damageAmount;
-        if(HP <= 0)
+        if (HP <= 0)
         {
             animator.SetTrigger("Die");
+            GetComponent<Collider>().enabled = false;
         }
         else
         {
             animator.SetTrigger("Damage");
         }
     }
-    
+
 }
